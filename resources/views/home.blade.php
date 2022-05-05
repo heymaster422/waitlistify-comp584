239ad1afef092f4db026b8ea3d6f1b87bf800019
Oscar Cansino <oscar.cansino.457@my.csun.edu>
2022-05-05 00:14:50 -0700

    home page changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,45 @@
         <!-- Content Block -->
         <div class="container">
             <h1> Home Page</h1>
+            <h3>
+                What is this website?
+            </h3>
             <p>
-                What is waitlistify? A website that can track classes students are interested in taking/waitlisting so
-                the CSUN Computer Science department can have a concrete number on how many
-                students are taking a class and prepare accordingly.
+                Waitlistify is a simple website that can track classes students are interested in taking/waitlisting so
+                that the CSUN Computer Science department can have a concrete number on how many
+                students are interested in taking a particular class. 
+            </p>
+            <h3>
+                Why was this created?
+            </h3>
+            <p>
+                It began with a dream and a vision, a utopian future where Computer Science students no longer have to fight tooth and nail to
+                enroll in the vital classes they need. No longer will students have to delay their graduation, nor would they have to resort to 
+                begging professors on their hands and knees for the most valuable resource mankind has ever sought: permission numbers. To
+                inspire future generations to one day achieve our dream, we created this website to serve as a template for what a companion
+                website for the CSUN enrollment service could look like.   
+            </p>
+            <h3>
+                How does this website work?
+            </h3>
+            <p>
+                Users of this website can log in and enroll in classes they are interested in.
+                They can also view statistics on how many students are interested as well, and
+                view which ones are highly sought after.  
+            </p>
+            <h3>
+                What was used to make this website?
+            </h3>
+            <p>
+                This website was made with a combination of Laravel, Docker, and SQL. We used Visual Studio Code to write it, and we stored it
+                on github.  
+            </p>
+            <h3>
+                Who made this website?
+            </h3>
+            <p>
+                This website was made through the combined powers of Oscar Cansino, Andrew Wong, and Michael Barseghian. We are Computer Science Majors at
+                California State University Northridge, and made this at the time of our last semester before graduating.  
             </p>
             <!--This is the description of our website. -->
             
